6754e61f54f7090ae8b50a16ef4ac1562345be3a
Xin Nie <xnie@misterbell.com>
2015-03-30 12:48:51 +0200

    handle callbacks

---
+++ simpleHTTPserver/blog_recent.js
@@ -0,0 +1,37 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function (req, res) {
+  getTitles(res);
+}).liten(8000, "127.0.0.1");
+
+function getTitles(res) {
+  fs.readFile('./titles.json', function (err, data) {
+    if (err) {
+      hadError(err, res);
+    } else {
+      getTemplate(JSON.parse(date.toString()), res);
+    }
+  })
+}
+
+function getTemplate(titles, res) {
+  fs.readFile('./template.html', function (err, data) {
+    if (err) {
+      hadError(err, res);
+    } else {
+      formatHtml(titles, data.toString(), res);
+    }
+  })
+}
+
+function formatHtml(titles, tmpl, res) {
+  var html = tmpl.replace('%', titles.join('</li><li>'));
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(html);
+}
+
+function hadError(err, res) {
+  console.error(err);
+  res.end('Server Error');
+}
