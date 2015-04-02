eeca4c793a8cdfa190be63025ca6119b75e03dba
Xin Nie <xnie@misterbell.com>
2015-04-02 10:52:43 +0200

    rss done

--- random_story/random_story.js
+++ random_story/random_story.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
-var request = request('request');
+var request = require('request');
 var htmlparser = require('htmlparser');
-var configFilename = './ress_feeds.txt';
+var configFilename = './rss_feeds.txt';
 
 function checkForRSSFile() {
   fs.exists(configFilename, function(exists) {
@@ -25,6 +25,38 @@ function readRSSFile(configFilename) {
 
 function downloadRSSFeed (feedUrl) {
   request({uri: feedUrl}, function(err, res, body) {
+    if (err) return next(err);
+    if (res.statusCode != 200)
+      return next(new Error('Abnormal response status code'));
 
-  })
+    next(null, body);
+  });
 }
+
+function parseRSSFeed (rss) {
+  var handler = new htmlparser.RssHandler();
+  var parser = new htmlparser.Parser(handler);
+  parser.parseComplete(rss);
+
+  if (!handler.dom.items.length)
+    return next(new Error('No RSS items found'));
+
+  var item = handler.dom.items.shift();
+  console.log(item.title);
+  console.log(item.link);
+}
+
+var tasks = [ checkForRSSFile, readRSSFile, downloadRSSFeed, parseRSSFeed];
+
+function next(err, result) {
+  if (err) throw err;
+
+  var currentTask = tasks.shift();
+
+  if (currentTask) {
+    currentTask(result);
+  }
+}
+
+next();
+
