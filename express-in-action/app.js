bb0c4efa3e84dd3d13c45de3539f3d7266c353a8
Xin Nie <xnie@misterbell.com>
2015-04-15 16:24:30 +0200

    8.4.2

--- express-in-action/app.js
+++ express-in-action/app.js
@@ -30,6 +30,8 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/', photos.list);
+app.get('/upload', photos.form);
+app.post('/upload', photos.submit(app.get('photos')));
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
